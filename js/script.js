8a085ee667f7df33fc3dba8c01207c251b735c09
hentschellj <hentschellj@gmail.com>
2020-02-09 15:19:44 -0600

    Slider completed and optimized

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
 $(document).ready(function() {
-  const sliderBox = $('.slider ul');
-  const width = $('.slider ul li').first().children('img').width();
-  const imageCount = $('.slider ul').children('li').length;
+  const sliderBox = $('.slider ul'),
+        width = $('.slider ul li').first().children('img').width(),
+        imageCount = $('.slider ul').children('li').length;
+  let currentImage = 1;
+
   sliderBox.css('width', width * imageCount);
   $('.nav button').on('click', function() {
     const whichButton = $(this).data('nav');
-    console.log(whichButton);
+    if (whichButton === 'next') {
+      if (currentImage === imageCount) {
+        currentImage = 1;
+        sliderTransition(currentImage, width);
+      } else {
+        currentImage++;
+        sliderTransition(currentImage, width);
+      }
+    } else if (whichButton === 'prev') {
+      if (currentImage === 1) {
+        currentImage = imageCount;
+        sliderTransition(currentImage, width);
+      } else {
+        currentImage--;
+        sliderTransition(currentImage, width);
+      }
+    }
   });
+
+  function sliderTransition(currentImageInput, widthInput) {
+    const pxValue = -(currentImageInput - 1) * widthInput;
+    sliderBox.animate({
+      'left': pxValue
+    });
+  }
+
 });
